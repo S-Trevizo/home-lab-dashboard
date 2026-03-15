582bd6f5fd20ec5cbe92f5addf5c38be92359001
S-Trevizo <strevizo@protonmail.com>
2026-03-14 21:30:33 -0500

    fix: update calibre color

--- src/data/services.ts
+++ src/data/services.ts
@@ -56,7 +56,7 @@ export const SERVICES: Service[] = [
     url: "https://books.xeon.quest",
     checkUrl: "https://books.xeon.quest",
     icon: "/calibre_icon.svg",
-    accentColor: "#d95ccc"
+    accentColor: "#d7a2d1"
   },
   {
     name: "Tautulli",
